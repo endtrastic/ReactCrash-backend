7ad591d5e05a563cdcda937fdbe9e95973991a16
endtrastic <a.tuvikene@voco.ee>
2024-11-25 15:12:20 +0200

    Added another item, created dummy_expenses

--- src/App.jsx
+++ src/App.jsx
@@ -1,28 +1,42 @@
 import './App.css'
 import Expenses from './components/Expenses/Expenses.jsx'
 import NewExpense from './components/Expenses/NewExpense/NewExpense.jsx'
+import { useState } from 'react'
+
+
+const DUMMY_EXPENSES = [
+  {
+    id: 'id1',
+    date: new Date(2024, 10, 12),
+    title: 'How to get 50000 dollars book',
+    price: 50000,
+  },
+  {
+    id: 'id2',
+    date: new Date(2024, 10, 12),
+    title: '(Brick) jeans',
+    price: 0.99,
+  },
+  {
+    id: 'id3',
+    date: new Date(2025, 10, 12),
+    title: 'Prisma Paper Bag',
+    price: -1000,
+  }
+
+]
 
 
 function App() {
+  const [expenses, setExpenses] = useState(DUMMY_EXPENSES)
 
-  const expenses = [
-    {
-      date: new Date(2024, 10, 12),
-      title: 'How to get 50000 dollars book',
-      price: 50000,
-      id: 1
-    },
-    {
-      date: new Date(2024, 10, 12),
-      title: '(Brick) jeans',
-      price: 0.99,
-      id: 2
-    }
-  ]
 
   const addExpenseHandler = (expense) => {
     console.log('In App.js')
     console.log(expense)
+    setExpenses((previousExpenses) => {
+      return [expense, ...previousExpenses]
+    })
   }
 
   return (
