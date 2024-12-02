9bd94ee75fbca0d4c4ad10ce14a5dd5ef52c899a
endtrastic <a.tuvikene@voco.ee>
2024-12-02 12:52:34 +0200

    fixed Filtering

--- src/components/Expenses/Expenses.jsx
+++ src/components/Expenses/Expenses.jsx
@@ -1,30 +1,27 @@
-import ExpensesFilter from './ExpensesFilter/ExpensesFilter.jsx'
+import ExpensesFilter from './ExpensesFilter/ExpensesFilter.jsx';
 import Card from '../UI/Card.jsx';
 import './Expenses.css';
 import ExpensesList from './ExpensesList/ExpensesList.jsx';
 import { useState } from 'react';
 
-
 const Expenses = (props) => {
     const [filteredYear, setFilteredYear] = useState('2024');
 
-
     const saveYearHandler = (selectedYear) => {
-        console.log('Selected Year in Expenses.jsx:', selectedYear)
-        setFilteredYear(selectedYear)
+        console.log('Selected Year in Expenses.jsx:', selectedYear);
+        setFilteredYear(selectedYear);
     }
 
-    
     const filteredExpenses = props.expenses.filter((expense) => {
-        return expense.date.getFullYear() === filteredYear
-    })
+        return expense.date.getFullYear().toString() === filteredYear;
+    });
 
     return (
         <Card className="expenses">
             <ExpensesFilter selected={filteredYear} onchangeyear={saveYearHandler}/>
             <ExpensesList expenses={filteredExpenses}/>
         </Card>
-    )
+    );
 }
 
 export default Expenses;
